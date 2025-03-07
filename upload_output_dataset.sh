7b652a8d6fe18b1328e9f0e565b75e55dbd5e06f
Martine Lenders <martine.lenders@tu-dresden.de>
2025-03-07 12:31:19 +0000

    Add upload script for data

---
+++ upload_output_dataset.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# upload_output_dataset.sh
+# Copyright (C) 2025 TU Dresden
+#
+# Distributed under terms of the MIT license.
+#
+
+rsync --partial --progress -avz output_dataset/ male646f@dgw.zih.tu-dresden.de:/glw/netdresdat/private-dns-constrained-eval-mlenders/
