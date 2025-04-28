7b10cd1bcf08182902520066f247e30f1b7a6aa3
Martine S. Lenders <m.lenders@fu-berlin.de>
2025-04-28 11:51:08 +0000

    word2vec.py: final update

--- word2vec.py
+++ word2vec.py
@@ -8,6 +8,8 @@
 import multiprocessing
 import os
 import pathlib
+import sys
+import traceback
 
 from gensim.models import word2vec
 import numpy
@@ -32,66 +34,75 @@ DNS_FORMATS = ["dns_message", "dns_cbor"]
 
 
 def scenario2vec(scenario):
-    file = INPUT_PATH / f"{scenario}.training.csv.gz" 
-    print("Processing", str(file))
-    df = polars.read_csv(file, separator=";")
-    nibbles, byte_size = df["eth.payload"].str.len_chars().max(), 2
+    try:
+        file = INPUT_PATH / f"{scenario}.training.csv.gz" 
+        print("Processing", str(file))
+        df = polars.read_csv(file, separator=";")
+        nibbles, byte_size = df["eth.payload"].str.len_chars().max(), 2
 
-    df = df.with_columns(
-        polars.col("eth.payload").map_elements(
-            lambda msg: [
-                msg[i : i + byte_size]
-                for i in range(0, nibbles, byte_size)
-            ],
-            return_dtype=list[str],
+        assert (nibbles % byte_size) == 0
+        df = df.with_columns(
+            polars.col("eth.payload").map_elements(
+                lambda msg: [
+                    msg[i : i + byte_size]
+                    for i in range(0, nibbles, byte_size)
+                ],
+                return_dtype=list[str],
+            )
         )
-    )
 
-    model = word2vec.Word2Vec(
-        df["eth.payload"].to_list(),
-        workers=WORKERS,
-        vector_size=VECTOR_SIZE,
-        min_count=1,
-        window=3,
-        sg=0,
-    )
+        model = word2vec.Word2Vec(
+            df["eth.payload"].to_list(),
+            workers=WORKERS,
+            vector_size=VECTOR_SIZE,
+            min_count=1,
+            window=3,
+            sg=0,
+        )
 
-    df_vec = df.with_columns(
-        vector=polars.col("eth.payload").map_elements(
-            lambda words: numpy.concatenate(
-                [model.wv[word] for word in words]
+        df_vec = df.with_columns(
+            vector=polars.col("eth.payload").map_elements(
+                lambda words: numpy.concatenate(
+                    [model.wv[word] for word in words]
+                ).tolist(),
+                return_dtype=polars.List(polars.Float32),
             ),
-            return_dtype=polars.datatypes.List(
-                polars.datatypes.Array(polars.Float32, VECTOR_SIZE)
+            label=(polars.col("client.type") != "dns").cast(
+                polars.Int8
             ),
-        ),
-        label=(polars.col("client.type") != "dns").cast(
-            polars.Int8
-        ),
-    )[["vector", "label"]]
-    df_vec.write_parquet(
-        INPUT_PATH / f"{scenario}.vector.parquet",
-        compression="zstd",
-        compression_level=10,
-    )
-    print("Created", str(INPUT_PATH / f"{scenario}.vector.parquet"))
-    del df
-    del df_vec
+        )[["vector", "label"]]
+        df_vec.write_parquet(
+            INPUT_PATH / f"{scenario}.vector.parquet",
+            compression="zstd",
+            compression_level=10,
+        )
+        print("Created", str(INPUT_PATH / f"{scenario}.vector.parquet"))
+        del df
+        del df_vec
+    except Exception:
+        print(traceback.format_exc(), file=sys.stderr)
+        raise
 
 
-scenarios = []
-for data in DATA_FORMATS:
-    for dns in DNS_FORMATS:
-        for l2 in LINK_LAYERS:
-            for prot in PROTOCOLS:
-                for blk in BLOCKWISE:
-                    for stp in NETWORK_SETUPS:
-                        scenario = f"{prot}{l2}-{stp}_{data}_{dns}{blk}"
-                        file = INPUT_PATH / f"{scenario}.training.csv.gz"
-                        vector_file = INPUT_PATH / f"{scenario}.vector.parquet"
-                        if file.exists() and not vector_file.exists():
-                            scenarios.append(scenario)
+def main():
+    scenarios = []
+    for data in DATA_FORMATS:
+        for dns in DNS_FORMATS:
+            for l2 in LINK_LAYERS:
+                for prot in PROTOCOLS:
+                    for blk in BLOCKWISE:
+                        for stp in NETWORK_SETUPS:
+                            scenario = f"{prot}{l2}-{stp}_{data}_{dns}{blk}"
+                            file = INPUT_PATH / f"{scenario}.training.csv.gz"
+                            vector_file = INPUT_PATH / f"{scenario}.vector.parquet"
+                            if file.exists() and not vector_file.exists():
+                                scenarios.append(scenario)
+                            elif vector_file.exists():
+                                print(f"Skipping {file} since {vector_file} exists")
+
+    with multiprocessing.Pool(8 if WORKERS > 8 else WORKERS) as pool:
+        pool.map(scenario2vec, scenarios)
 
 
-with multiprocessing.Pool(5 if WORKERS > 5 else WORKERS) as pool:
-    pool.map(scenario2vec, scenarios)
+if __name__ == "__main__":
+    main()
