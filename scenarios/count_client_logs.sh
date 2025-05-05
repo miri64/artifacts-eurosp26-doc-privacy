0fbcc8dce0bdded8cf478e7cef5cb624eeac95fd
Martine Lenders <m.lenders@fu-berlin.de>
2025-05-05 17:17:21 +0200

    count_client_logs.sh: initial checker script

---
+++ scenarios/count_client_logs.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# count_client_logs.sh
+# Copyright (C) 2025 TU Dresden
+#
+# Distributed under terms of the MIT license.
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath "$0")" )" &> /dev/null && pwd )
+OUTPUT_DATASETS="$(readlink -f "${OUTPUT_DATASETS:-${SCRIPT_DIR}/../output_dataset}")"
+
+awk '
+    {
+        timestamps[FILENAME][$1 * 10**9] += 1
+    }
+    END {
+        for (fn in timestamps) {
+            sum = 0; min=0;
+            for (ts in timestamps[fn]) {
+                if ((!min || min > ts) && ts != 0) { min = ts }
+                sum += timestamps[fn][ts]
+            }
+            print min, fn, length(timestamps[fn]), sum
+        }
+    }' "${OUTPUT_DATASETS}"/*.client.log | \
+        sort -n | awk '{print $3, $4, $2}'
