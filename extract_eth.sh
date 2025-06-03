a1037a1bc3b2269233196697afd6da2248a0c80d
Martine Lenders <m.lenders@fu-berlin.de>
2025-06-03 22:52:08 +0200

    extract_eth.sh: also extract addresses and ethertype

--- extract_eth.sh
+++ extract_eth.sh
@@ -13,7 +13,7 @@ fi
 
 PCAP="$1"
 
-FIELDS="frame.number frame.time_epoch eth.payload"
+FIELDS="frame.number frame.time_epoch eth.src eth.dst eth.type eth.payload"
 echo "${FIELDS}" | \
     awk 'BEGIN {OFS="\t"} { for (i = 1; i <= NF; i++) { printf "%s%s", $i, (i < NF) ? OFS : ORS } }'
-tshark -Tfields -e frame.number -e frame.time_epoch -e data --disable-protocol ALL --enable-protocol eth -r "${PCAP}"
+tshark -Tfields -e frame.number -e frame.time_epoch -e eth.src -e eth.dst -e eth.type -e data --disable-protocol ALL --enable-protocol eth -r "${PCAP}"
