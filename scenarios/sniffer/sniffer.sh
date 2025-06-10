6922ca395aadda1884e180d769699998b837097c
Martine Lenders <m.lenders@fu-berlin.de>
2025-06-10 23:05:02 +0200

    sniffer: include SCHC_RULES_LOG in output file name

--- scenarios/sniffer/sniffer.sh
+++ scenarios/sniffer/sniffer.sh
@@ -23,9 +23,13 @@ elif [ "${SECURITY}" = "tls" ]; then
     NETWORK_SCENARIO="$(echo "${NETWORK_SCENARIO}" | sed 's/http/https/g')"
 fi
 
+if [ -n "${SCHC_RULES_MODE}" ]; then
+    SCHC_RULES_LOG="-${SCHC_RULES_MODE}"
+fi
+
 su - user -c "\
    /usr/bin/tshark \
    -i '${SNIFFER_IFACE}' \
    -f '${SNIFFER_FILTER}' \
-   -w '/dumps/${NETWORK_SCENARIO}_${DATA_FORMAT}_${DNS_FORMAT}${BLOCK_SIZE}.${SNIFFER_LOGNAME}.pcapng' \
+   -w '/dumps/${NETWORK_SCENARIO}${SCHC_RULES_LOG}_${DATA_FORMAT}_${DNS_FORMAT}${BLOCK_SIZE}.${SNIFFER_LOGNAME}.pcapng' \
 "
