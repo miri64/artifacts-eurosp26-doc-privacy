183ed7834b2bf99d298af9a5009bc09161fc41f2
Martine Lenders <martine.lenders@tu-dresden.de>
2025-06-16 12:06:33 +0000

    upload_output_dataset.sh: exclude some debugging logs

--- upload_output_dataset.sh
+++ upload_output_dataset.sh
@@ -6,4 +6,4 @@
 # Distributed under terms of the MIT license.
 #
 
-rsync --exclude *.coap.csv --exclude *.eth.csv --exclude *.merged.csv --exclude .gitignore --exclude .ipynb_checkpoints/ --partial --progress -avz output_dataset/ male646f@dgw.zih.tu-dresden.de:/glw/netdresdat/private-dns-constrained-eval-mlenders/
+rsync --exclude *.coap.csv --exclude *.eth.csv --exclude *.merged.csv --exclude .gitignore --exclude .ipynb_checkpoints/ --exclude docker-compose-*.log --exclude *.stderr.log --exclude *server.log --partial --progress -avz output_dataset/ male646f@dgw.zih.tu-dresden.de:/glw/netdresdat/private-dns-constrained-eval-mlenders/
