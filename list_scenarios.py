b63c17e58ad08f7cc47907dc97872e53ed4a7543
Martine S. Lenders <m.lenders@fu-berlin.de>
2025-07-04 12:40:37 +0000

    Provide common module to list scenarios

---
+++ list_scenarios.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright (C) 2025 TU Dresden
+#
+# Distributed under terms of the MIT license.
+
+import argparse
+
+
+PROTOCOLS = ["https", "coap", "coaps", "oscore", "oscore-base"]
+LINK_LAYERS = ["", "-schc"]
+LINK_LAYER_MODES = ["", "-min-rules", "-peer-based"]
+BLOCKWISE = ["", "_b64"]
+NETWORK_SETUPS = ["d1", "d2", "p1", "p2"]
+DATA_FORMATS = ["json", "cbor"]
+DNS_FORMATS = ["dns_message", "dns_cbor"]
+
+
+def list_scenarios(filter_protocol=None):
+    for data in DATA_FORMATS:
+        for dns in DNS_FORMATS:
+            for l2 in LINK_LAYERS:
+                for l2_mode in LINK_LAYER_MODES:
+                    if l2_mode and not l2:
+                        continue
+                    for prot in PROTOCOLS:
+                        if prot == "coap" and l2:
+                            continue
+                        if filter_protocol is not None and prot not in filter_protocol:
+                            continue
+                        for blk in BLOCKWISE:
+                            if prot == "https" and (blk or l2):
+                                continue
+                            for stp in NETWORK_SETUPS:
+                                if l2_mode == "-min-rules":
+                                    if not (
+                                        stp == "d2"
+                                        or (stp == "p2" and prot == "oscore-base")
+                                    ):
+                                        continue
+                                if l2_mode == "-peer-based" and stp != "d2":
+                                    continue
+                                yield f"{prot}{l2}-{stp}{l2_mode}_{data}_{dns}{blk}"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--protocol",
+        help="Protocol to train for (default: all)",
+        nargs="+",
+        default=None,
+        choices=PROTOCOLS,
+    )
+    args = parser.parse_args()
+    for scenario in list_scenarios(args.protocol):
+        print(scenario)
+
+
+if __name__ == "__main__":
+    main()
