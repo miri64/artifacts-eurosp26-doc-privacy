d47c502a01fd8bcd41ec805d67dabd69d538f838
Martine Lenders <m.lenders@fu-berlin.de>
2025-09-18 13:53:15 +0200

    count_client_logs.sh: make work with gzip'd logs

--- scenarios/count_client_logs.sh
+++ scenarios/count_client_logs.sh
@@ -9,18 +9,20 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath "$0")" )" &> /dev/null && pwd )
 OUTPUT_DATASETS="$(readlink -f "${OUTPUT_DATASETS:-${SCRIPT_DIR}/../output_dataset}")"
 
-awk '
-    {
-        timestamps[FILENAME][$1 * 10**9] += 1
-    }
-    END {
-        for (fn in timestamps) {
-            sum = 0; min=0;
-            for (ts in timestamps[fn]) {
-                if ((!min || min > ts) && ts != 0) { min = ts }
-                sum += timestamps[fn][ts]
-            }
-            print min, fn, length(timestamps[fn]), sum
+for log in "${OUTPUT_DATASETS}"/*.client.log "${OUTPUT_DATASETS}"/*.client.log.gz; do
+    awk '
+        {
+            timestamps[FILENAME][$1 * 10**9] += 1
         }
-    }' "${OUTPUT_DATASETS}"/*.client.log | \
-        sort -n | awk 'BEGIN {print "Requests", "Messages", "Log"} {print $3, $4, $2}'
+        END {
+            for (fn in timestamps) {
+                sum = 0; min=0;
+                for (ts in timestamps[fn]) {
+                    if ((!min || min > ts) && ts != 0) { min = ts }
+                    sum += timestamps[fn][ts]
+                }
+                print min, fn, length(timestamps[fn]), sum
+            }
+        }' "${log}"
+done | \
+    sort -n | awk 'BEGIN {print "Requests", "Messages", "Log"} {print $3, $4, $2}'
