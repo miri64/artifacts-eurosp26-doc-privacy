2a832a854799ef8b93cc59a43aff32c862bc018f
Martine Lenders <martine.lenders@tu-dresden.de>
2025-09-18 12:12:11 +0000

    extract_coap.sh: Add new fields to help find headers to randomize

--- extract_coap.sh
+++ extract_coap.sh
@@ -14,10 +14,10 @@ fi
 PCAP="$1"
 
 FIELDS="frame.number frame.time_epoch frame.protocols dtls.record.content_type"
-FIELDS="${FIELDS} coap.code coap.request_first_in coap.mid coap.token coap.opt.ctype coap.opt.block_number coap.opt.block_mflag coap.opt.block_size coap.block coap.block.reassembled.in"
+FIELDS="${FIELDS} coap.code coap.request_first_in coap.mid coap.token coap.opt.uri_host coap.opt.ctype coap.opt.block_number coap.opt.block_mflag coap.opt.block_size coap.block coap.block.reassembled.in"
 
 if echo "$PCAP" | grep -q "oscore" || [ "$2" = "oscore" ]; then
-    FIELDS="${FIELDS} oscore.code oscore.opt.ctype oscore.opt.block_number oscore.opt.block_mflag oscore.opt.block_size"
+    FIELDS="${FIELDS} oscore.code oscore.opt.ctype oscore.opt.block_number oscore.opt.block_mflag oscore.opt.block_size coap.opt.object_security_piv"
 fi
 
 
