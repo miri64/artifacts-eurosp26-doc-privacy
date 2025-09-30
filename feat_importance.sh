d1a62c149a85c041f5a167859bf676559a6743f5
Martine Lenders <m.lenders@fu-berlin.de>
2025-09-30 16:55:41 +0200

    Add feature importance determination scripts

---
+++ feat_importance.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# feat_importance.sh
+# Copyright (C) 2025 TU Dresden
+#
+# Distributed under terms of the MIT license.
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath "$0")" )" &> /dev/null && pwd )
+PROCS=$(grep -c '^processor' /proc/cpuinfo)
+if [ $PROCS -gt 8 ]; then
+    # Cap to keep in memory limits
+    PROCS=8
+fi
+INPUT_PATH="${INPUT_PATH:-${SCRIPT_DIR}/output_dataset}"
+
+feat_importance() {
+    export POLARS_FORCE_NEW_STREAMING=1
+    export INPUT_PATH
+    ./feat_importance.py "$1"
+}
+
+export -f feat_importance
+export INPUT_PATH
+echo "Running on ${PROCS} workers"
+./list_scenarios.py $* | parallel -j "${PROCS}" feat_importance
