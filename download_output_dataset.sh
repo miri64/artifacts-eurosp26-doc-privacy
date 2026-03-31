392b0c01495b8b5615c7698543a6abfb1e285b61
Martine Lenders <martine.lenders@tu-dresden.de>
2026-03-31 17:52:55 +0000

    Remove download and upload scripts
    They only lead to TU-Dresden-internal servers...

--- download_output_dataset.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# upload_output_dataset.sh
-# Copyright (C) 2025 TU Dresden
-#
-# Distributed under terms of the MIT license.
-#
-
-rsync --exclude .gitignore --partial --progress -avz male646f@dgw.zih.tu-dresden.de:/glw/netdresdat/private-dns-constrained-eval-mlenders/ output_dataset/
--- upload_output_dataset.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# upload_output_dataset.sh
-# Copyright (C) 2025 TU Dresden
-#
-# Distributed under terms of the MIT license.
-#
-
-rsync --exclude *.coap.csv --exclude *.eth.csv --exclude *.merged.csv --exclude .gitignore --exclude .ipynb_checkpoints/ --exclude docker-compose-*.log --exclude *.stderr.log --exclude *server.log --partial --progress -avz output_dataset/ male646f@dgw.zih.tu-dresden.de:/glw/netdresdat/private-dns-constrained-eval-mlenders/
